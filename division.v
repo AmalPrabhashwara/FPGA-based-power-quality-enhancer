2a28f05843bdec7c75b56cc94732cebc91497c0f
AmalPrabhashwara <43935159+AmalPrabhashwara@users.noreply.github.com>
2020-12-17 17:43:14 +0530

    Create division.v

---
+++ division.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/06/2020 10:39:10 AM
+// Design Name: 
+// Module Name: DIVISION2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DIVISION2(
+A,Res
+    );
+
+ parameter WIDTH = 56;
+    //input and output ports.
+    input [WIDTH-1:0] A;
+    wire [WIDTH-1:0] B=26;
+    output [WIDTH-1:0] Res;
+    //internal variables    
+    reg [WIDTH-1:0] Res = 0;
+    reg [WIDTH-1:0] a1,b1;
+    reg [WIDTH:0] p1;   
+    integer i;
+
+    always@ (A or B)
+    begin
+        //initialize the variables.
+        a1 = A;
+        b1 = B;
+        p1= 0;
+        for(i=0;i < WIDTH;i=i+1)    begin //start the for loop
+            p1 = {p1[WIDTH-2:0],a1[WIDTH-1]};
+            a1[WIDTH-1:1] = a1[WIDTH-2:0];
+            p1 = p1-b1;
+            if(p1[WIDTH-1] == 1)    begin
+                a1[0] = 0;
+                p1 = p1 + b1;   end
+            else
+                a1[0] = 1;
+        end
+        Res = a1;   
+    end 
+    
+    
+endmodule
